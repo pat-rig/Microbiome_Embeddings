4cdb869f1aa6588d18b609a34bd1aa349d60947b
pat-rig <pakoehle@gmail.com>
2021-04-01 10:11:30 +0200

    find bug in sorting

--- prediction_experiments/backtrace_signal.py
+++ prediction_experiments/backtrace_signal.py
@@ -12,6 +12,7 @@ with open('imp_asv_tables.obj', mode='rb') as table_file:
     tables = pickle.load(table_file)
     table_file.close()
     
+<<<<<<< HEAD
     # Only execute on machine with large memory
 # =============================================================================
 # # only execute on large memory machine (file requires ~1GB)
@@ -32,14 +33,24 @@ with open('imp_asv_tables.obj', mode='rb') as table_file:
 #     
 # =============================================================================
     # if on local machine: collect abundances
+=======
+# only execute on large memory machine (file requires ~1GB)
+# load abundance data 
+abundance_data = pd.read_csv('../data/seqtab_filter.07.txt', sep='\t', index_col=0)
+>>>>>>> df70e0dd81a0b6928037872bcc13cd7c3b8d34ba
 
 
 with open('sign_abundances.obj', mode='rb') as abundance_file:
     sign_abundances = pickle.load(abundance_file)
     abundance_file.close()
+<<<<<<< HEAD
 
 # first look compute variances per column
 raw_variances = sign_abundances['raw'].var(axis=0).values
 pca_variances = sign_abundances['pca'].var(axis=0).values    
 
     
+=======
+    
+    
+>>>>>>> df70e0dd81a0b6928037872bcc13cd7c3b8d34ba
